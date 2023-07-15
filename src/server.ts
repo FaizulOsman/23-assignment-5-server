e856fa7ce0a32cdc2a13ec004c319320a2990ec8
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-15 10:59:32 +0600

    feat: database connection done

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
-const express = require("express");
+import express from "express";
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 5000;
+
+// Database Connection
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(`Failed to connect database`, error);
+  }
+}
+main();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
